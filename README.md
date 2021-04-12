fd20d755574bba44ee22660b1e0cf559ea924f2d
Anton <antvoronkoff@gmail.com>
2021-04-12 17:21:33 +0300

    Add batch file for build and run application

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # sea_battle
 It's simple small game's project for console fun :)
+
+Launch Start.bat for gameplay
---
+++ Start.bat
@@ -0,0 +1,3 @@
+cd .
+javac -sourcepath ./src -d bin src/com/antvoronkoff/SeaBattle/Main.java
+java -classpath ./bin com.antvoronkoff.seabattle.Main
